e35e40addcac4d42f054cf32feb5341eed6c5ba2
SonuThapaMagar <sonumagar787@gmail.com>
2025-01-23 21:28:46 +0545

    Navbar changed

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
 // Importing Navbar
-import Navbar from "./components/Navbar"; 
+import Navbar from "./components/Navbar";
 //Importing Hero Section
 import Hero from "./components/Hero";
-import Profile from "./assets/sonu.png";
-import ProfileBg from "./assets/Ellipse 1.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,8 +12,8 @@ function App() {
     <>
       {/* Navbar */}
       <Navbar />
-
-      <Hero/>
+      {/* Hero Section */}
+      <Hero />
     </>
   );
 }
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,51 +1,66 @@
 import React, { useState } from 'react';
+import {Link} from "react-scroll";
+import {FaTimes} from "react-icons/fa";
+import {CiMenuFries} from "react-icons/ci";
+
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
+  const [click,setClick]=useState(false);
+  const handleClick=()=>{
+    setClick(!click);
+  }
+  const content = <>
+    <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900">
+      <ul className='text-center text-xl p-20'>
+        <Link spy={true} smooth={true} to="Home">
+          <li className='hover:text-fuchsia-600 transition border-b border-slate-900 hover:border-fuchsia-600 cursor-pointer'>Home</li>
+        </Link>
+        <Link spy={true} smooth={true} to="About">
+          <li className='hover:text-fuchsia-600 transition border-b border-slate-900 hover:border-fuchsia-600 cursor-pointer'>About</li>
+        </Link>
+        <Link spy={true} smooth={true} to="Project">
+          <li className='hover:text-fuchsia-600 transition border-b border-slate-900 hover:border-fuchsia-600 cursor-pointer'>Project</li>
+        </Link>
+        <Link spy={true} smooth={true} to="Blogs">
+          <li className='hover:text-fuchsia-600 transition border-b border-slate-900 hover:border-fuchsia-600 cursor-pointer'>Blogs</li>
+        </Link>
+      </ul>
+    </div>
+  </>
   return (
-    <header className="bg-dark-background sticky top-0 z-20 mx-auto w-full items-center justify-between overflow-x-hidden">
-      <div className="container mx-auto px-4 py-4 w-full">
-        <div className="flex justify-between items-center w-full my-4">
-          <div>
-            <h1 className="font-bold text-xl">m.Sonu</h1>
-          </div>
-          <div className="block lg:hidden">
-            <button onClick={toggleMenu} className="text-gray-400 hover:text-white">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </button>
-          </div>
-          <div className={`lg:flex space-x-6 ${isMenuOpen ? 'block' : 'hidden'}`}>
-            <ul className="flex space-x-6">
-              <li>
-                <a href="#home" className="text-gray-400 hover:text-white">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#about" className="text-gray-400 hover:text-white">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#projects" className="text-gray-400 hover:text-white">
-                  Project
-                </a>
-              </li>
-              <li>
-                <a href="#blogs" className="text-gray-400 hover:text-white">
-                  Blogs
-                </a>
-              </li>
+    <nav>
+      <div className='h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4'>
+        <div className='flex items-center flex-1'>
+          <span className='text-3xl font-bold'>m.Sonu</span>
+        </div>
+        <div className='lg:flex mg:flex lg:flex-1 items-center justify-end font-normal hidden'>
+          <div className='flex-10'>
+            <ul className='flex gap-8 mr-16 text-[18ox]'>
+              <Link spy={true} smooth={true} to="Home">
+                <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Home</li>
+              </Link>
+              <Link spy={true} smooth={true} to="About">
+                <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>About</li>
+              </Link>
+              <Link spy={true} smooth={true} to="Project">
+                <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Project</li>
+              </Link>
+              <Link spy={true} smooth={true} to="Blogs">
+                <li className='my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Blogs</li>
+              </Link>
             </ul>
           </div>
         </div>
+
+        <div>
+          {click && content}
+        </div>
+
+        <button className='block sm:hidden trasntion' onClick={handleClick}>
+          {click ? <FaTimes/> : <CiMenuFries/> }
+        </button>
       </div>
-    </header>
+    </nav>
   );
 };
 
