28e570a8ee79c68a4d748bd3c848366130c951b5
SonuThapaMagar <sonumagar787@gmail.com>
2025-01-24 16:02:46 +0545

    Footer added

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,43 +1,44 @@
 import React from "react";
 import Profile from "../assets/sonu.png";
+import { FaLinkedin, FaGithub, FaInstagram, FaFacebook } from "react-icons/fa";
 
 const Hero = () => {
-    return (
-        <main>
-            {/* Introduction Section Started */}
-            <section>
-                <div className="container m-32 my-10 px-4 flex flex-col lg:flex-row items-center gap-4">
-                    {/* Left Content */}
-                    <div className="lg:w-1/2 text-center lg:text-left space-y-4">
-                        <h1 className="font-bold text-4xl">Hello, I'm</h1>
-                        <h1 className="font-bold text-6xl py-3 gradient-text">
-                            Sonu Thapa Magar
-                        </h1>
-                        <p className="mt-4 text-gray-400">
-                            Tech Enthusiast || Undergraduate University Student
-                        </p>
-                        {/* Download Resume Button */}
-                        <a
-                            href="/resume.pdf" // The path to the resume PDF in the public folder
-                            download
-                            className="mt-6 inline-block bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 transition duration-300"
-                        >
-                            Download Resume
-                        </a>
-                    </div>
+  return (
+    <main>
+      {/* Introduction Section Started */}
+      <section>
+        <div className="container m-32 my-10 px-4 flex flex-col lg:flex-row items-center gap-4">
+          {/* Left Content */}
+          <div className="lg:w-1/2 text-center lg:text-left space-y-4">
+            <h1 className="font-bold text-4xl">Hello, I'm</h1>
+            <h1 className="font-bold text-6xl py-3 gradient-text">
+              Sonu Thapa Magar
+            </h1>
+            <p className="mt-4 text-gray-400">
+              Tech Enthusiast || Undergraduate University Student
+            </p>
+            {/* Download Resume Button */}
+            <a
+              href="/resume.pdf" // The path to the resume PDF in the public folder
+              download
+              className="mt-6 inline-block bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 transition duration-300"
+            >
+              Download Resume
+            </a>
+          </div>
 
-                    {/* Right Image */}
-                    <div className="lg:w-1/2 flex justify-center">
-                        <img
-                            src={Profile}
-                            alt="Sonu Thapa Magar"
-                            className="w-full max-w-sm lg:max-w-md object-cover rounded-lg shadow-lg"
-                        />
-                    </div>
-                </div>
-            </section>
-            {/* Introduction Section Ended */}
-            {/* <section>
+          {/* Right Image */}
+          <div className="lg:w-1/2 flex justify-center">
+            <img
+              src={Profile}
+              alt="Sonu Thapa Magar"
+              className="w-full max-w-sm lg:max-w-md object-cover rounded-lg shadow-lg"
+            />
+          </div>
+        </div>
+      </section>
+      {/* Introduction Section Ended */}
+      {/* <section>
                 <div className="container m-auto flex flex-col items-center justify-center min-h-screen text-center">
                     <div className="glitch animate-glitch animate-noiseAfter relative font-geologica font-bold text-white z-10 tracking-tight text-4xl whitespace-nowrap" data-text="Who is Sonu?">
                         Who is Sonu?
@@ -63,8 +64,66 @@ const Hero = () => {
 
                 </div>
             </section> */}
-        </main>
-    );
+      <div className="flex justify-center mx-auto">
+        <div>
+          <h1 className="text-4xl font-bold font-Montserrat py-4 mx-auto">
+            <span className="gradient-text">CONNECT</span> ME ON
+          </h1>
+          <p className="text-center">
+            Feel free to <span className="gradient-text">connect</span> me
+          </p>
+          <div className="flex justify-center gap-6 py-4 mb-4">
+            {/* LinkedIn */}
+            <a
+              href="https://www.linkedin.com/in/sonu-thapa-magar-b281b8275/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white-600 hover:text-blue-300 transition duration-300"
+            >
+              <FaLinkedin size={40} />
+            </a>
+
+            {/* GitHub */}
+            <a
+              href="https://github.com/SonuThapaMagar"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white-800 hover:text-blue-300 transition duration-300"
+            >
+              <FaGithub size={40} />
+            </a>
+
+            {/* Instagram */}
+            <a
+              href="https://www.instagram.com/_soucie_/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white-800 hover:text-blue-300  transition duration-300"
+            >
+              <FaInstagram size={40} />
+            </a>
+
+            {/* Facebook */}
+            <a
+              href="https://www.facebook.com/sonu.thapamagar.9083"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white-800 hover:text-blue-300  transition duration-300"
+            >
+              <FaFacebook size={40} />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer Section */}
+      <footer className="bg-gray-900 text-gray-400 py-6 text-center">
+        <p className="text-sm">
+          &copy;{new Date().getFullYear()} Sonu Thapa Magar. All rights reserved.
+        </p>
+      </footer>
+    </main>
+  );
 };
 
 export default Hero;
