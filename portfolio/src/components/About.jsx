97b721ecf381b8f424d695680a8e4d90178a4f6a
SonuThapaMagar <sonumagar787@gmail.com>
2025-02-01 13:39:05 +0545

    Tech and tools section added

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import sonu from "../assets/sonu.png";
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaGitAlt, FaGithub, FaPython } from "react-icons/fa";
+import { FaHtml5,FaFigma, FaCss3Alt, FaJs, FaReact, FaJava, FaGitAlt, FaGithub, FaPython ,FaJira} from "react-icons/fa";
+import { VscVscode } from "react-icons/vsc";
 
 const techStack = [
   { name: "HTML", icon: <FaHtml5 className="text-orange-500" /> },
-]
+  { name: "CSS", icon: <FaCss3Alt className="text-blue-500" /> },
+  { name: "JavaScript", icon: <FaJs className="text-yellow-500" /> },
+  { name: "React", icon: <FaReact className="text-cyan-500" /> },
+  { name: "Java", icon: <FaJava className="text-blue-500" /> },
+  { name: "Python", icon: <FaPython className="text-blue-400" /> },
+];
+const tools = [
+  { name: "GitHub", icon: <FaGithub className="text-white-700" /> },
+  { name: "Git", icon: <FaGitAlt className="text-red-500" /> },
+  { name: "VS Code", icon: <VscVscode className="text-blue-500" /> },
+  { name: "Figma", icon: <FaFigma className="text-purple-500" /> },
+  { name: "Jira", icon: <FaJira className="text-blue-700" /> },
+
+];
 
 const About = () => {
   return (
@@ -57,12 +71,25 @@ const About = () => {
 
       {/* Tech Stack Section */}
       <div className="mt-16 max-w-6xl mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-6">Tech Stack & Tools</h2>
+        <h2 className="text-3xl font-bold mb-6"><strong className='gradient-text'>Tech</strong> Stack</h2>
         <div className="grid grid-cols-2 md:grid-cols-5 gap-6">
           {techStack.map((tech, index) => (
             <div key={index} className="flex flex-col items-center p-4 rounded-lg shadow-md hover:scale-105 transition">
               <div className="text-5xl">{tech.icon}</div>
               <p className="mt-2 text-lg font-semibold">{tech.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Tools Section */}
+      <div className="mt-16 max-w-6xl mx-auto text-center">
+        <h2 className="text-3xl font-bold mb-6"><strong className='gradient-text'>Tools</strong></h2>
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-6">
+          {tools.map((tool, index) => (
+            <div key={index} className="flex flex-col items-center p-4 rounded-lg shadow-md hover:scale-105 transition">
+              <div className="text-5xl">{tool.icon}</div>
+              <p className="mt-2 text-lg font-semibold">{tool.name}</p>
             </div>
           ))}
         </div>
